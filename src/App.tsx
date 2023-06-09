13d54bf1526f448342bc4be915f6c49abe7d948d
FGeaorg <g29f05@gmail.com>
2023-06-09 23:12:10 +0800

    Настройка переменных стилей, темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import { Suspense } from "react";
-import { Link } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
-import { Counter } from "./components/Counter"
-import './index.scss';
+import { Suspense, useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import './styles/index.scss';
 
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.NORMAL);
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.NORMAL ? Theme.DARK : Theme.NORMAL);
+  }
 
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button
+        onClick={toggleTheme}
+      >TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
